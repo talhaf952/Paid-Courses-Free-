5dde38c3fc13fb4936509f2ec647886e0c2ed394
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-09-15 09:08:42 +0500

    Update

---
+++ Next.js
@@ -0,0 +1,19 @@
+🔥🔥The *Ultimate Next.js 14 Course + Ebook* 🔥🔥
+
+
+Become a top 1% Next.js developer & lead the Next era of React
+
+*Duration 20 hours*
+
+💰Course Price*: $74*💰
+
+*🆓🆓 But Free For Everyone 🆓🆓*
+
+*Demo Link:*
+https://www.jsmastery.pro/ultimate-next-course
+
+*Download Link*
+https://mega.nz/folder/yFsnmTSB
+
+*Decryption Key*
+```U9Dh7CFopMf6f4QVzOi-9Q```
